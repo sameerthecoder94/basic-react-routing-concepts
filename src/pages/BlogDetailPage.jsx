8c17cbe7c1b10e1bf6e8f034c0408378cac171d8
sameerthecoder94 <sameerthecoder@gmail.com>
2022-04-08 11:54:26 -0400

    useSearchParams() for topRated query string

--- src/pages/BlogDetailPage.jsx
+++ src/pages/BlogDetailPage.jsx
@@ -1,8 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import { blogs } from '../data';
 
 function BlogDetailPage() {
   const params = useParams(); // get the dynamic ID from the URL
+  const [searchParams] = useSearchParams(); // get the query string from the URL
+
+  const topRated = searchParams.get('topRated'); // get the topRated query string from the URL
 
   // logic that extract the article based on the `params.dynamicID`
   const blogDetail = blogs.find(
@@ -13,6 +16,18 @@ function BlogDetailPage() {
 
   return (
     <div>
+      {topRated === 'yes' ? (
+        <div
+          style={{
+            marginTop: 20,
+            backgroundColor: 'coral',
+            display: 'inline-block',
+          }}
+        >
+          <code>Top Rated</code>
+        </div>
+      ) : null}
+
       <h1>{blogDetail.title}</h1>
       <p>{blogDetail.description}</p>
     </div>
